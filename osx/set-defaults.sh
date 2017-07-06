e64573181e95040c91f664a9b635cb6559b91985
Alexander Lee <alexander.lee@usc.edu>
2017-07-06 15:01:38 -0400

    Update set-defaults.sh

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -105,8 +105,8 @@ defaults -currentHost write -g com.apple.trackpad.trackpadCornerClickBehavior -i
 defaults -currentHost write com.apple.trackpad.enableSecondaryClick -bool true
 
 # Set a really fast keyboard repeat rate.
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 10
 
 # Disable press-and-hold for keys in favor of key repeat.
 # defaults write -g ApplePressAndHoldEnabled -bool false
